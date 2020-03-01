e1be7f4454c0b00eec9bd7a0877e98f4d318ad54
Berger Patrik, ENT-BPN-HLT-DO (EXT) <Patrik.Berger@swisscom.com>
2020-03-01 11:05:53 +0100

    sure fire, unit test

--- xls-converter-core-poi/src/test/java/com/erste/acme/xlsconverter/core/poi/RowMapperImplTest.java
+++ xls-converter-core-poi/src/test/java/com/erste/acme/xlsconverter/core/poi/RowMapperImplTest.java
@@ -1,8 +1,15 @@
 package com.erste.acme.xlsconverter.core.poi;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
 
+import com.erste.acme.commons.domain.entity.Delivery;
+import java.util.Date;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 class RowMapperImplTest {
 
@@ -10,6 +17,25 @@ class RowMapperImplTest {
 
     @Test
     void mapToDelivery() {
+        Date now = new Date();
+        Row row = getTestRow(now);
 
+        Delivery delivery = rowMapper.mapToDelivery(row);
+
+        assertEquals(100, delivery.getAmount());
+        assertEquals("testProduct", delivery.getProduct().getName());
+        assertEquals(now, delivery.getDate());
+    }
+
+    private Row getTestRow(Date date) {
+        Row row = Mockito.mock(Row.class);
+        Cell cell = Mockito.mock(Cell.class);
+
+        when(cell.getStringCellValue()).thenReturn("testProduct");
+        when(cell.getNumericCellValue()).thenReturn(100.0);
+        when(cell.getDateCellValue()).thenReturn(date);
+        when(row.getCell(anyInt())).thenReturn(cell);
+
+        return row;
     }
 }
